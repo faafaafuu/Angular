5b1875bfc4ff36db7f48c8b99dc26013c8471978
farzullaev@bk.ru <farzullaev@bk.ru>
2018-06-26 16:12:19 +0300

    Component structure

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+//Декоратор описывающий св-ва компонента
 @Component({
+  // Методанные
   selector: 'app-root',
+  // Файл шаблона
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Определение логики Component 
 export class AppComponent {
-  title = 'app';
+  title = 'app'; 
 }
