b410cb27c06a2c2fce111d4202777c86bb7fd75a
farzullaev@bk.ru <farzullaev@bk.ru>
2018-06-27 01:24:11 +0300

    Directives

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   // Методанные
   // <tag> внутри которого будет разворачиваться компонент, 
   //root - указывает на то, что этот элемент является корневым и все остальные компоненты строятся кнутри этого элемента
+  // 'app-root' - кастомная директива
   selector: 'app-root',
   // Файл шаблона
   templateUrl: './app.component.html',
@@ -12,7 +13,14 @@ import { Component } from '@angular/core';
 })
 // Определение логики Component 
 export class AppComponent {
-  title = 'my app';  
+  title = 'my app';
+  // Cв-во типа Array, any - в качестве типа данных ( позволяет поместить этот массив данные люб.тип.)
+  users: Array<any> = [
+    {name: 'One'},
+    {name: 'Two'},
+    {name: 'Three'},
+    {name: 'Four'}
+  ];    
   onBtnClick() {
     return this.title = 'That\'s how it working Event Binding'
   }
